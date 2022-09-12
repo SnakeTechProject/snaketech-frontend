3e290bb7d8d2ee4d2ccb782d3a423d527221439b
Monica <contatomonicavaz@gmail.com>
2022-09-12 15:20:01 -0300

    chore: sass config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${path.resolve(
+          __dirname,
+          './src/styles/variables',
+        )}";`,
+      },
+    },
+  },
 });
