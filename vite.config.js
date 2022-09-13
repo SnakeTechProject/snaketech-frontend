a806376ee6e3ffcea13a5ce31085a063ef3860a6
murilo muller <murilomuller@protonmail.com>
2022-09-13 12:12:11 -0300

    chore: format routes for windows and linux

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
+/*
+Config antiga, estava dando erro de importação do scss, config atualizada pelo @cosmos no windows e revisada por min (murilo) no linux, necessita de revisão de outras pessoas/sistemas  (após isso esse comentarios pode ser excluido)
+*/
+
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+const additionalDataDir = resolve(__dirname, './src/styles/variables');
+
+const replaceDir = (dir) => dir.replace(/[\\]/g, '/');
+const isWindows = () => process.platform.includes('win');
+
 export default defineConfig({
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "${path.resolve(
-          __dirname,
-          './src/styles/variables',
-        )}";`,
+        additionalData: `@import "${
+          isWindows() ? replaceDir(additionalDataDir) : additionalDataDir
+        }";`,
       },
     },
   },
