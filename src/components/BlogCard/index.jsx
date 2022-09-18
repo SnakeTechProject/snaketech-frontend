9c28717f29b67b97ef04e4ddb35a147d85d8ff5c
VMatiasDev <vitormatiasdev@gmail.com>
2022-09-18 18:52:39 -0300

    refactor: adds proptypes to blogcard component

--- src/components/BlogCard/index.jsx
+++ src/components/BlogCard/index.jsx
@@ -1,18 +1,27 @@
-// import React from 'react'
-import styles from './blogCard.module.scss'
+import PropTypes from 'prop-types';
+import styles from './blogCard.module.scss';
 
-export function BlogCard(props) {
-  const {img, title, short} = props.card
-  return (<div className={styles.cardContainer}>
-    <img className={styles.imgCard}
-    src={img.imgUrl}
-    alt={img.title} />
-    <div className={styles.textContainer}>
-      <div className={styles.textTitle}>{title}</div>
-      <div className={styles.textShort}>{short}</div>
+export function BlogCard({imgUrl, title, short}) {
+  return (
+    <div className={styles.cardContainer}>
+      <img
+        src={imgUrl}
+        alt={title}
+        className={styles.imgCard}
+      />
+      <div className={styles.textContainer}>
+        <div className={styles.textTitle}>{title}</div>
+        <div className={styles.textShort}>{short}</div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.btn}>Leia mais</button>
+      </div>
     </div>
-    <div className={styles.buttonContainer}>
-      <button className={styles.btn}>Leia mais</button>
-    </div>
-  </div>)
+  );
 }
+
+BlogCard.propTypes = {
+  imgUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  short: PropTypes.string.isRequired,
+};
