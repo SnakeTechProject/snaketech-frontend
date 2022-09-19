1a1c71ee40461144c2a505472b4762135fcbb6fb
Monica <contatomonicavaz@gmail.com>
2022-09-19 20:59:12 -0300

    feat: add input component and reset password page

--- src/pages/ResetPassword/index.jsx
+++ src/pages/ResetPassword/index.jsx
@@ -1,9 +1,25 @@
 import styles from './resetpassword.module.scss';
+import { Input } from '../../components/Input';
 
 export function ResetPassword() {
+  function handleSubmit() {}
+
   return (
     <div className={styles.wrapper}>
-      <h1>Resetar senha SnakeTech</h1>
+      <h1 className={styles.title}>Atualização de senha</h1>
+
+      <form onSubmit={handleSubmit}>
+        <Input
+          title="Digite sua nova senha"
+          placeholder="Nova senha"
+          type="password"
+        />
+        <Input title="Repita a senha" type="password" />
+
+        <button type="submit" className={styles.submitBtn}>
+          Cadastre-se
+        </button>
+      </form>
     </div>
   );
 }
