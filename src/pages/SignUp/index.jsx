b80c0ad09a66037aee901813afe4d9fb24f7ed1a
Monica <contatomonicavaz@gmail.com>
2022-09-22 12:54:22 -0300

    feat: adds signup page, minor scss changes

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,9 +1,33 @@
+import { Input } from '../../components/Input';
 import styles from './signup.module.scss';
 
 export function SignUp() {
   return (
     <div className={styles.wrapper}>
-      <h1>SignUp SnakeTech</h1>
+      <div className={styles.titleWrapper}>
+        <h1 className={styles.title}>
+          Cadastre-se e faça parte da comunidade SnakeTech
+        </h1>
+        <img src="/assets/img/registerLogo.png" alt="" />
+      </div>
+
+      <form className={styles.inputWrapper}>
+        <Input title="Nome" required />
+        <Input title="E-mail" type="email" required />
+        <Input
+          title="Crie sua senha"
+          placeholder="Criar senha"
+          type="password"
+          required
+        />
+        <Input
+          title="Repita a senha"
+          placeholder="Repetir senha"
+          type="password"
+          required
+        />
+        <button className={styles.filledBtn}>Cadastre-se</button>
+      </form>
     </div>
   );
 }
