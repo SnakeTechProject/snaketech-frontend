2d528a846386082b5dab23dca035f574c469668c
vicentecarlos <81314892+VicenteCarlos@users.noreply.github.com>
2022-09-27 09:03:05 -0300

    feature/form-home-page (#15)
    * style: create new header
    
    * feat: implements layout responsive on header and menu-hamburger
    
    * refactor: implements files mockeds
    
    * feat: implements form on homePage
    
    * style: formHomePage
    
    Co-authored-by: Gilbertoans <anschaugilberto@gmail.com>
    Co-authored-by: Dan <dansenpirdev@gmail.com>

---
+++ src/assets/mocks/index.js
@@ -0,0 +1,66 @@
+const cardsBlog = [
+  {
+    imgUrl: '/assets/img/cardPython.png',
+    title: 'Você conhece o Python?',
+    short:
+      'Python é uma linguagem Open-Source de propósito geral usado bastante em data science, machine learning...',
+  },
+  {
+    imgUrl: '/assets/img/cardCss.png',
+    title: 'O que é CSS?',
+    short:
+      'função do CSS é justamente otimizar o aspecto visual das páginas, de uma maneira mais organizada.',
+  },
+  {
+    imgUrl: '/assets/img/cardFront.png',
+    title: 'O que faz um Front-end?',
+    short:
+      ' É o desenvolvimento da interface gráfica do usuário de um site, por meio do uso de HTML, CSS e JavaScript,',
+  },
+];
+
+const cardsNews = [
+  {
+    imgUrl: '/assets/img/cardIphone.png',
+    text: 'iPhone 14: 3 inovações que a Apple trouxe em sua nova linha de smartphones',
+  },
+  {
+    imgUrl: '/assets/img/cardSecurity.png',
+    text: '5 dicas de segurança para sua vida digital',
+  },
+  {
+    imgUrl: '/assets/img/cardWaze.png',
+    text: 'Usuários do Waze relatam instabilidade no aplicativo nesta terça-feira',
+  },
+];
+
+const cardsJobs = [
+  {
+    role: 'UX DEsign (Discovery) na Vivo',
+    time: 'Tempo integral · Pleno-sênior',
+  },
+  {
+    role: 'DEV java (pl/sr) Solutis Tecnologias',
+    time: 'Tempo integral · Assistente',
+  },
+];
+
+const cardsViews = [
+  {
+    text: '1 - Você conhece o Python?',
+  },
+  {
+    text: '2 - O que faz um Front-end?',
+  },
+  {
+    text: '3 - O que é CSS?',
+  },
+];
+
+const inputsAndLabels = [
+  ['Nome*', 'Nome', 'Qual o seu nome?'],
+  ['E-mail*', 'E-mail', 'Seu melhor e-mail'],
+  ['Mensagem*', 'Mensagem', 'Digite sua mensagem'],
+];
+
+export { cardsBlog, cardsJobs, cardsNews, cardsViews, inputsAndLabels };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,95 +5,80 @@ import { JobsCard } from '../../components/JobsCard';
 import { TopViewsCard } from '../../components/TopViewsCard';
 
 import styles from './home.module.scss';
+import {
+  cardsBlog,
+  cardsJobs,
+  cardsNews,
+  cardsViews,
+  inputsAndLabels,
+} from '../../assets/mocks';
 
 export function Home() {
-  const cardsBlog = [
-    {
-      imgUrl: '/assets/img/cardPython.png',
-      title: 'Você conhece o Python?',
-      short: 'Python é uma linguagem Open-Source de propósito geral usado bastante em data science, machine learning...'
-    },
-    {
-      imgUrl: '/assets/img/cardCss.png',
-      title: 'O que é CSS?',
-      short: 'função do CSS é justamente otimizar o aspecto visual das páginas, de uma maneira mais organizada.'
-    },
-    {
-      imgUrl: '/assets/img/cardFront.png',
-      title: 'O que faz um Front-end?',
-      short: ' É o desenvolvimento da interface gráfica do usuário de um site, por meio do uso de HTML, CSS e JavaScript,'
-    }
-  ];
-
-  const cardsNews = [
-    {
-      imgUrl: '/assets/img/cardIphone.png',
-      text: 'iPhone 14: 3 inovações que a Apple trouxe em sua nova linha de smartphones'
-    },
-    {
-      imgUrl: '/assets/img/cardSecurity.png',
-      text: '5 dicas de segurança para sua vida digital'
-    },
-    {
-      imgUrl: '/assets/img/cardWaze.png',
-      text: 'Usuários do Waze relatam instabilidade no aplicativo nesta terça-feira'
-    }
-  ];
-
-  const cardsJobs = [
-    {
-      role: 'UX DEsign (Discovery) na Vivo',
-      time: 'Tempo integral · Pleno-sênior'
-    },
-    {
-      role: 'DEV java (pl/sr) Solutis Tecnologias',
-      time: 'Tempo integral · Assistente'
-    }
-  ];
-
-  const cardsViews = [
-    {
-      text: '1 - Você conhece o Python?'
-    },
-    {
-      text: '2 - O que faz um Front-end?'
-    },
-    {
-      text: '3 - O que é CSS?'
-    }
-  ];
-
   return (
     <div className={styles.wrapper}>
       <h1>Home SnakeTech</h1>
-      <Challenge/>
+      <Challenge />
 
-      <div className={styles.cardsBlogContainer}>
-        {cardsBlog.map((card, index) => <BlogCard imgUrl={card.imgUrl} title={card.title} short={card.short} key={index} />)}
-      </div>
+      <section className={styles.cardsBlogContainer}>
+        {cardsBlog.map((card, index) => (
+          <BlogCard
+            imgUrl={card.imgUrl}
+            title={card.title}
+            short={card.short}
+            key={index}
+          />
+        ))}
+      </section>
 
-      <div className={styles.apiContainer}>
+      <section className={styles.apiContainer}>
         <div className={styles.newsContainer}>
           <h1>Se liga na news!</h1>
           <div className={styles.cardsNewsContainer}>
-            {cardsNews.map((card, index) => <NewsCard imgUrl={card.imgUrl} text={card.text} key={index} />) }
+            {cardsNews.map((card, index) => (
+              <NewsCard imgUrl={card.imgUrl} text={card.text} key={index} />
+            ))}
           </div>
         </div>
 
         <div className={styles.jobsContainer}>
           <h1>Olha a vaga!</h1>
           <div className={styles.cardsJobsContainer}>
-            {cardsJobs.map((card, index) => <JobsCard role={card.role} time={card.time} key={index} />) }
+            {cardsJobs.map((card, index) => (
+              <JobsCard role={card.role} time={card.time} key={index} />
+            ))}
           </div>
         </div>
 
         <div className={styles.topViewContainer}>
           <h1>Os mais vistos</h1>
           <div className={styles.cardsTopViews}>
-            {cardsViews.map((card, index) => <TopViewsCard text={card.text} key={index} />) }
+            {cardsViews.map((card, index) => (
+              <TopViewsCard text={card.text} key={index} />
+            ))}
           </div>
         </div>
-      </div>
+      </section>
+
+      <section className={styles.formHomePage}>
+        <img
+          src="https://cdn.discordapp.com/attachments/1021558762487693404/1022945971937738844/Business_man_holding_a_laptop_with_project_tasks_on_screen.jpg"
+          alt="character"
+        />
+        <h2>Dúvidas e sugestões? Entre em contato conosco</h2>
+        <form>
+          {inputsAndLabels.map((item, i) => (
+            <>
+              <fieldset key={i}>
+                <label key={i + 1} htmlFor={item[1]}>
+                  {item[0]}
+                </label>
+                <input key={i + 2} id={item[1]} placeholder={item[2]} />
+              </fieldset>
+            </>
+          ))}
+          <button type="submit">Enviar</button>
+        </form>
+      </section>
     </div>
   );
 }
